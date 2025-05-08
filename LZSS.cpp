372b15b7f594ce0dfb5e9a88a57fc7061c786a16
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2025-05-08 17:00:30 -0500

    code completed

--- LZSS.cpp
+++ LZSS.cpp
@@ -3,29 +3,50 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-int main(){
-    std::string message = "HELLO";
-    std::vector<char> search_buffer;
 
 
-
-    for (int i=0;i<search_buffer.size();++i){
-        std::cout << search_buffer[i] << " ";
-        
-    }
-
-    for(uint i=0;i<message.size();i++){
-        auto it = std::find(search_buffer.begin(),search_buffer.end(),message[i]);
-        if(it != search_buffer.end()){
-            size_t index = std::distance(search_buffer.begin(),it);
-            std::cout << "Found at index" <<index <<std::endl;
+int elements_in_array(const std::vector<char>& check_elements, const std::vector<char>& elements) {
+    int i = 0;
+    int offset = 0;
+    for (int x = 0; x < elements.size(); x++) {
+        if (offset >= check_elements.size()) {
+            return i - check_elements.size();
         }
-            std::cout << "Found: " << message[i] << std::endl;
-       search_buffer.push_back(message[i]);
+        if (check_elements[offset] == elements[x]) {
+            offset += 1;
+        } else {
+            offset = 0;
+        }
+        i += 1;
+    }
+    return -1; 
+}
+
+int main() {
+    std::string message = "SAM SAM";
+    std::vector<char> search_buffer;
+    std::vector<char> check_characters;
+
+    for (uint i = 0; i < message.size(); i++) {
+        check_characters.push_back(message[i]);
+        
+        int index = elements_in_array(check_characters, search_buffer);
+        if (index == -1 || i == message.size() - 1) {
+            if (check_characters.size() > 1) {
+                std::vector<char> sub_check_characters(check_characters.begin(), check_characters.end() - 1);
+                index = elements_in_array(sub_check_characters, search_buffer);
+                int offset = i - index - check_characters.size() + 1;
+                int length = check_characters.size();
+                std::cout << "<" << offset << ", " << length << ">" << std::endl;
+            } else {
+                std::cout << message[i] << std::endl;
+            }
+            check_characters.clear();
+        }
+        
+        search_buffer.push_back(message[i]);
     }
 
-
-
-    std::cout <<std::endl;
+    std::cout << std::endl;
     return 0;
 }
